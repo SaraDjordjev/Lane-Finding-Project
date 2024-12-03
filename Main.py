8f072a1372d50667be96e3ba29708b38963a857e
SaraDjordjev <134224437+SaraDjordjev@users.noreply.github.com>
2024-12-03 12:04:11 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -17,31 +17,39 @@ def process(image):
     edges = process_canny(combined_mask, thr1=30, thr2=200)
 
     # Dodavanje tacaka za perspektivnu transformaciju
-    src = np.float32([
-        [600, 460],
-        [720, 460],
-        [1140, 720],
-        [220, 720]
-    ])
-    dst = np.float32([
-        [300, 0],
-        [1000, 0],
-        [1000, 720],
-        [300, 720]
-    ])
+    width = image.shape[1]
+    height = image.shape[0]
+
+    # src
+    bottom_left = [round(width * 0.15), round(height * 0.99)]
+    bottom_right = [round(width * 0.9), round(height * 0.99)]
+    top_left = [round(width * 0.43), round(height * 0.65)]
+    top_right = [round(width * 0.59), round(height * 0.65)]
+
+    src = np.float32([bottom_left, bottom_right, top_right, top_left])
+
+    # dst
+    bottom_left = [0, height]
+    bottom_right = [width, height]
+    top_left = [0, height * 0.25]
+    top_right = [width, height * 0.25]
+
+    dst = np.float32([bottom_left, bottom_right, top_right, top_left])
 
     warped_image, points_image, M, Minv = perspective_transform(edges, src, dst)
     # Detekcija piksela trake i fitovanje
     left_fit, right_fit, ploty, left_fitx, right_fitx = detect_lane_pixels_and_fit(warped_image, visualize=True)
 
-    # Warp lane back onto the original image
+    # Transformacija u originalni prikaz
     lane_overlay = warp_back(undistorted_image, warped_image, left_fitx, right_fitx, ploty, Minv)
 
     return undistorted_image, filtered_image, edges, warped_image, points_image, left_fitx, right_fitx, ploty, lane_overlay
 
 def main():
 
+
     input_path = 'test_videos/project_video03.mp4'
+    #input_path = 'test_images/straight_lines2.jpg'
     is_video = input_path.endswith(('.mp4', '.avi', '.mov'))  # Provera da li je ulaz video
 
     if is_video:
@@ -56,7 +64,7 @@ def main():
         fps = int(cap.get(cv.CAP_PROP_FPS))
         frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-        out = cv.VideoWriter('output_video.avi', fourcc, fps, (frame_width, frame_height))
+        out = cv.VideoWriter('examples/output_video.avi', fourcc, fps, (frame_width, frame_height))
 
         while True:
             ret, frame = cap.read()
@@ -65,10 +73,7 @@ def main():
 
             # Procesiranje okvira
             results = process(frame)
-            _, _, _, warped_image, _, _, _, _, lane_overlay = results
-            plt.imshow(warped_image, cmap='gray')
-            plt.title("Ulazna binarna slika")
-            plt.show()
+            _, _, _, _, _, _, _, _, lane_overlay = results
 
             # Prikaz rezultata
             cv.imshow('Lane Detection', lane_overlay)
@@ -92,23 +97,9 @@ def main():
         results = process(image)
         undistorted, filtered, edges, warped, points_image, left_fitx, right_fitx, ploty, lane_overlay = results
 
-
         # Prikaz rezultata
-        cv.imshow('Original', image)
-        cv.imshow('Filtered Image', filtered)
-        cv.imshow('Edges', edges)
-        cv.imshow('Warped Image', warped)
-        cv.imshow('Points Image', points_image)
-
-
-        cv.imwrite('Binary.jpg', edges)
-        cv.imwrite('warped_image.jpg', warped)
-
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-
         cv.imshow('Lane Detection', lane_overlay)
-        cv.imwrite('output_image.jpg', lane_overlay)
+        cv.imwrite('examples/output_image.jpg', lane_overlay)
         print("Procesiranje slike zavrseno.")
         cv.waitKey(0)
         cv.destroyAllWindows()
